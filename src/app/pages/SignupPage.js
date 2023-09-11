a4f03da6fda9e983048de5ea7e32299757d89fd4
subhadipshee <subhadipshee20010521@gmail.com>
2023-09-11 11:40:14 +0530

    cart-and react-router-dom add

---
+++ src/app/pages/SignupPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Signup from '../../features/auth/components/SignupPage';
+
+const SignupPage = () => {
+  return (
+    <div>
+      <Signup/>
+    </div>
+  );
+}
+
+export default SignupPage;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,jsx,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/forms"),
+  ],
+};
+
